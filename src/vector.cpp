edb7d2a328bc42735c0ac1b238f6b6960cc85848
LouisAsanaka <25756888+LouisAsanaka@users.noreply.github.com>
2025-02-16 22:12:17 -0600

    Inlined vector functions

--- src/vector.cpp
+++ src/vector.cpp
@@ -49,68 +49,9 @@ void Vector::operator/=(const int rhs) {
   z /= rhs;
 }
 
-Vector Vector::operator-(const Vector &rhs) const {
-  return Vector(x - rhs.x, y - rhs.y, z - rhs.z);
-}
-Vector Vector::operator+(const Vector &rhs) const {
-  return Vector(x + rhs.x, y + rhs.y, z + rhs.z);
-}
 /*
 Vector Vector::operator * (const Vector a) {
    return Vector(y*a.z-z*a.y, z*a.x-x*a.z, x*a.y-y*a.x);
 }*/
-Vector Vector::operator*(const double rhs) const {
-  return Vector(x * rhs, y * rhs, z * rhs);
-}
-Vector Vector::operator*(const float rhs) const {
-  return Vector(x * rhs, y * rhs, z * rhs);
-}
-Vector Vector::operator*(const int rhs) const {
-  return Vector(x * rhs, y * rhs, z * rhs);
-}
-Vector Vector::operator/(const double rhs) const {
-  return Vector(x / rhs, y / rhs, z / rhs);
-}
-Vector Vector::operator/(const float rhs) const {
-  return Vector(x / rhs, y / rhs, z / rhs);
-}
-Vector Vector::operator/(const int rhs) const {
-  return Vector(x / rhs, y / rhs, z / rhs);
-}
-Vector Vector::cross(const Vector &a) const {
-  return Vector(y * a.z - z * a.y, z * a.x - x * a.z, x * a.y - y * a.x);
-}
-double Vector::mag2() const { return x * x + y * y + z * z; }
-double Vector::mag() const { return sqrt(x * x + y * y + z * z); }
-double Vector::dot(const Vector &a) const {
-  return x * a.x + y * a.y + z * a.z;
-}
-Vector Vector::normalize() const {
-  double m = mag();
-  return Vector(x / m, y / m, z / m);
-}
-
-Vector solveScalers(const Vector &v1, const Vector &v2, const Vector &v3,
-                    const Vector &C) {
-  double denom = v1.z * v2.y * v3.x - v1.y * v2.z * v3.x - v1.z * v2.x * v3.y +
-                 v1.x * v2.z * v3.y + v1.y * v2.x * v3.z - v1.x * v2.y * v3.z;
-  double a = C.z * v2.y * v3.x - C.y * v2.z * v3.x - C.z * v2.x * v3.y +
-             C.x * v2.z * v3.y + C.y * v2.x * v3.z - C.x * v2.y * v3.z;
-  double b = -C.z * v1.y * v3.x + C.y * v1.z * v3.x + C.z * v1.x * v3.y -
-             C.x * v1.z * v3.y - C.y * v1.x * v3.z + C.x * v1.y * v3.z;
-  double c = C.z * v1.y * v2.x - C.y * v1.z * v2.x - C.z * v1.x * v2.y +
-             C.x * v1.z * v2.y + C.y * v1.x * v2.z - C.x * v1.y * v2.z;
-  return Vector(a / denom, b / denom, c / denom);
-}
-
-// Add new optimized version that uses cached values
-Vector solveScalersCached(const Vector &C, double cachedDenom,
-                          const Vector &coeffsA, const Vector &coeffsB,
-                          const Vector &coeffsC) {
-  double a = C.x * coeffsA.x + C.y * coeffsA.y + C.z * coeffsA.z;
-  double b = C.x * coeffsB.x + C.y * coeffsB.y + C.z * coeffsB.z;
-  double c = C.x * coeffsC.x + C.y * coeffsC.y + C.z * coeffsC.z;
-  return Vector(a / cachedDenom, b / cachedDenom, c / cachedDenom);
-}
 
 Ray::Ray(const Vector &po, const Vector &ve) : point(po), vector(ve) {}
--- src/vector.h
+++ src/vector.h
@@ -14,29 +14,52 @@ public:
   Vector() : x(0.0), y(0.0), z(0.0) {}
   Vector(double a, double b, double c);
 
-  void operator+=(const Vector &);
-  void operator-=(const Vector &);
-  void operator*=(const double);
-  void operator*=(const float);
-  void operator*=(const int);
-  void operator/=(const double);
-  void operator/=(const float);
-  void operator/=(const int);
+  inline void operator+=(const Vector &);
+  inline void operator-=(const Vector &);
+  inline void operator*=(const double);
+  inline void operator*=(const float);
+  inline void operator*=(const int);
+  inline void operator/=(const double);
+  inline void operator/=(const float);
+  inline void operator/=(const int);
 
-  Vector operator+(const Vector &) const;
-  Vector operator-(const Vector &) const;
+  inline Vector operator+(const Vector &rhs) const {
+    return Vector(x + rhs.x, y + rhs.y, z + rhs.z);
+  }
+  inline Vector operator-(const Vector &rhs) const {
+    return Vector(x - rhs.x, y - rhs.y, z - rhs.z);
+  }
   /*  Vector operator * (const Vector);*/
-  Vector operator*(const double) const;
-  Vector operator*(const float) const;
-  Vector operator*(const int) const;
-  Vector operator/(const double) const;
-  Vector operator/(const float) const;
-  Vector operator/(const int) const;
-  Vector cross(const Vector &a) const;
-  double mag2() const;
-  double mag() const;
-  double dot(const Vector &a) const;
-  Vector normalize() const;
+  inline Vector operator*(const double rhs) const {
+    return Vector(x * rhs, y * rhs, z * rhs);
+  }
+  inline Vector operator*(const float rhs) const {
+    return Vector(x * rhs, y * rhs, z * rhs);
+  }
+  inline Vector operator*(const int rhs) const {
+    return Vector(x * rhs, y * rhs, z * rhs);
+  }
+  inline Vector operator/(const double rhs) const {
+    return Vector(x / rhs, y / rhs, z / rhs);
+  }
+  inline Vector operator/(const float rhs) const {
+    return Vector(x / rhs, y / rhs, z / rhs);
+  }
+  inline Vector operator/(const int rhs) const {
+    return Vector(x / rhs, y / rhs, z / rhs);
+  }
+  inline Vector cross(const Vector &a) const {
+    return Vector(y * a.z - z * a.y, z * a.x - x * a.z, x * a.y - y * a.x);
+  }
+  inline double mag2() const { return x * x + y * y + z * z; }
+  inline double mag() const { return sqrt(x * x + y * y + z * z); }
+  inline double dot(const Vector &a) const {
+    return x * a.x + y * a.y + z * a.z;
+  }
+  inline Vector normalize() const {
+    double m = mag();
+    return Vector(x / m, y / m, z / m);
+  }
 };
 
 class Ray {
@@ -45,40 +68,42 @@ public:
   Ray(const Vector &po, const Vector &ve);
 };
 
-inline Vector operator-(const Vector b) { return Vector(-b.x, -b.y, -b.z); }
+inline Vector operator-(const Vector &b) { return Vector(-b.x, -b.y, -b.z); }
 
-inline Vector operator+(const Vector b) { return b; }
+inline Vector operator+(const Vector &b) { return b; }
 
-inline Vector operator*(const int a, const Vector b) {
+inline Vector operator*(const int a, const Vector &b) {
   return Vector(a * b.x, a * b.y, a * b.z);
 }
 
-inline Vector operator*(const double a, const Vector b) {
+inline Vector operator*(const double a, const Vector &b) {
   return Vector(a * b.x, a * b.y, a * b.z);
 }
 
-inline Vector operator*(const float a, const Vector b) {
+inline Vector operator*(const float a, const Vector &b) {
   return Vector(a * b.x, a * b.y, a * b.z);
 }
 
-inline Vector operator/(const int a, const Vector b) {
+inline Vector operator/(const int a, const Vector &b) {
   return Vector(a / b.x, a / b.y, a / b.z);
 }
 
-inline Vector operator/(const double a, const Vector b) {
+inline Vector operator/(const double a, const Vector &b) {
   return Vector(a / b.x, a / b.y, a / b.z);
 }
 
-inline Vector operator/(const float a, const Vector b) {
+inline Vector operator/(const float a, const Vector &b) {
   return Vector(a / b.x, a / b.y, a / b.z);
 }
 
-Vector solveScalers(const Vector &v1, const Vector &v2, const Vector &v3,
-                    const Vector &solve);
-
-Vector solveScalersCached(const Vector &C, double cachedDenom,
-                          const Vector &coeffsA, const Vector &coeffsB,
-                          const Vector &coeffsC);
+inline Vector solveScalersCached(const Vector &C, double cachedDenom,
+                                 const Vector &coeffsA, const Vector &coeffsB,
+                                 const Vector &coeffsC) {
+  double a = C.x * coeffsA.x + C.y * coeffsA.y + C.z * coeffsA.z;
+  double b = C.x * coeffsB.x + C.y * coeffsB.y + C.z * coeffsB.z;
+  double c = C.x * coeffsC.x + C.y * coeffsC.y + C.z * coeffsC.z;
+  return Vector(a / cachedDenom, b / cachedDenom, c / cachedDenom);
+}
 
 int print_vector(FILE *stream, const struct printf_info *info,
                  const void *const *args);
