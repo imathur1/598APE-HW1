6f7d2644bf79b87ba8eb7ece38b36dc31db9c568
LouisAsanaka <25756888+LouisAsanaka@users.noreply.github.com>
2025-02-17 00:41:06 -0600

    Added elephant (sphere 200x200) test case

---
+++ run_tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./test.sh pianoroom 500 500
+./test.sh elephant 200 200
--- test.sh
+++ test.sh
@@ -2,14 +2,16 @@
 set -e
 
 FILENAME=${1:-pianoroom}
+H=${2:-500}
+W=${3:-500}
 
 CORRECT_ICON="\U2705"
 WRONG_ICON="\U274C"
 
-./main.exe -i "inputs/$FILENAME.ray" --ppm -o "output/$FILENAME.ppm" -H 500 -W 500
+./main.exe -i "inputs/$FILENAME.ray" --ppm -o "output/$FILENAME.ppm" -H $H -W $W
 
 if cmp --silent -- "output/$FILENAME.ppm" "output/${FILENAME}_correct.ppm" ; then
-  echo -e "$CORRECT_ICON $FILENAME passed"
+  echo -e "$CORRECT_ICON $FILENAME ($H x $W) passed"
 else
-  echo -e "$WRONG_ICON $FILENAME failed"
+  echo -e "$WRONG_ICON $FILENAME ($H x $W) failed"
 fi
