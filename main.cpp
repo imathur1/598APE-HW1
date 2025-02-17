12eb706d7783bd95d04dc80ff6bfbf1184db93de
LouisAsanaka <25756888+LouisAsanaka@users.noreply.github.com>
2025-02-17 00:58:30 -0600

    merge with openMP commit

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@
 #include "src/box.h"
 #include "src/disk.h"
 #include "src/triangle.h"
-#include "src/Textures/imagetexture.h"
-#include "src/Textures/colortexture.h"
-#include<stdio.h>
-#include<stdlib.h>
+#include "src/vector.h"
+#include <gperftools/profiler.h>
+#include <iostream>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <iostream>
 #include <gperftools/profiler.h>
@@ -47,12 +49,14 @@ void set(int i, int j, unsigned char r, unsigned char g, unsigned char b){
    DATA[3*(i+j*W)+2] = b; 
 }
 
-void refresh(Autonoma* c){
-   for(int n = 0; n<H*W; ++n) 
-   { 
-      Vector ra = c->camera.forward+((double)(n%W)/W-.5)*((c->camera.right))+(.5-(double)(n/W)/H)*((c->camera.up));
-      calcColor(&DATA[3*n], c, Ray(c->camera.focus, ra), 0);
-   }
+void refresh(Autonoma *c) {
+#pragma omp parallel for schedule(dynamic)
+  for (int n = 0; n < H * W; ++n) {
+    Vector ra = c->camera.forward +
+                ((double)(n % W) / W - .5) * ((c->camera.right)) +
+                (.5 - (double)(n / W) / H) * ((c->camera.up));
+    calcColor(&DATA[3 * n], c, Ray(c->camera.focus, ra), 0);
+  }
 }
 
 void outputPPM(FILE* f){
