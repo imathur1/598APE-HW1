005e8a408a9019e757de06337a41b3f54abd9bd7
LouisAsanaka <25756888+LouisAsanaka@users.noreply.github.com>
2025-02-16 20:14:28 -0600

    More Makefile targets + CI

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ FILENAME=${1:-pianoroom}
 CORRECT_ICON="\U2705"
 WRONG_ICON="\U274C"
 
-./main.exe -i "inputs/$FILENAME.ray" --ppm -o "output/$FILENAME.ppm" -H 500 -W 500
+taskset -c 3 ./main.exe -i "inputs/$FILENAME.ray" --ppm -o "output/$FILENAME.ppm" -H 500 -W 500
 
 if cmp --silent -- "output/$FILENAME.ppm" "output/${FILENAME}_correct.ppm" ; then
   echo -e "$CORRECT_ICON $FILENAME passed"
